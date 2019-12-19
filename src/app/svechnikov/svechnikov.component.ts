d14b9f94f67fec8be19e23c2b773d49de7adfb21
holmesr19 <holmesr19@mail.wlu.edu>
2019-12-19 00:04:00 -0500

    setting up svech page

--- src/app/svechnikov/svechnikov.component.ts
+++ src/app/svechnikov/svechnikov.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SvechnikovService} from './svechnikov.service'
 @Component({
   selector: 'app-svechnikov',
   templateUrl: './svechnikov.component.html',
   styleUrls: ['./svechnikov.component.scss']
 })
 export class SvechnikovComponent implements OnInit {
-
-  constructor() { }
+svech: ISvech;
+  constructor(private svechService: SvechnikovService) { }
 
   ngOnInit() {
+    console.log('in svech component');
+    this.svechService.getSvech('8480830')
+    .subscribe((data) => {this.svech = data;
+                          console.log(this.svech)})
   }
 
 }
