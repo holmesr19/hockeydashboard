fa62ef472c69ccb788f4c9f7bda19a261bc98e30
holmesr19 <holmesr19@mail.wlu.edu>
2020-11-17 22:57:27 -0500

    getting historical teams, roster causing an issue

--- src/app/historicalplayers/historicalplayers.component.ts
+++ src/app/historicalplayers/historicalplayers.component.ts
@@ -28,6 +28,7 @@ export class HistoricalplayersComponent implements OnInit {
     this.showPlayerCards = false;
     this.playerService.getYears()
     .subscribe((data) => this.years = data );
+    console.log(this.years);
   }
 
   getTeams(year: string) {
@@ -41,6 +42,7 @@ export class HistoricalplayersComponent implements OnInit {
     this.showPlayerCards = false;
     this.playerService.getRoster(year, teamId)
     .subscribe((data) => this.players = data );
+    console.log(this.players);
   }
 
   getProfile(playerId: string, year: string) {
--- src/app/services/historical-player.service.ts
+++ src/app/services/historical-player.service.ts
@@ -4,22 +4,51 @@ import { Team } from 'src/app/interfaces/Team';
 import { Player } from 'src/app/interfaces/Player';
 import { PersonExtended } from 'src/app/interfaces/PersonExtended';
 import { StatsObj } from 'src/app/interfaces/StatsObj';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/internal/operators/map';
+import { TeamRes } from 'src/app/interfaces/TeamRes';
+import { TeamRoster } from 'src/app/interfaces/TeamRoster';
+import { PlayerStats } from 'src/app/interfaces/PlayerStats';
+import { PersonRes } from 'src/app/interfaces/PersonRes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoricalPlayerService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   getYears(): Observable<string[]> {
-    return of(['19171918']);
+    let firstYear = 1917;
+    let secondYear = 1918;
+    const years = [];
+    const thisYear = new Date().getFullYear();
+    while (firstYear < thisYear) {
+      years.push(firstYear.toString() + secondYear.toString());
+      firstYear = secondYear;
+      secondYear++;
+    }
+    return of(years);
   }
-  getTeams(year: string): Observable<Team[]> {}
+  getTeams(year: string): Observable<Team[]> {
+    return this.httpClient.get<TeamRes>(
+      `https://statsapi.web.nhl.com/api/v1/teams?season=` + `${year}`
+    ).pipe(map(data => data.teams));
+  }
 
-  getRoster(year: string, teamId: string): Observable<Player[]> {}
+  getRoster(year: string, teamId: string): Observable<Player[]> {
+    return this.httpClient.get<TeamRoster>(
+      `https://statsapi.web.nhl.com/api/v1/teams/` + `${teamId}` + `?expand=team.roster&season=` + `${year}`
+    ).pipe(map(data => data.roster));
+  }
 
-  getPlayer(playerId: string): Observable<PersonExtended> {}
+  getPlayer(playerId: string): Observable<PersonExtended> {
+    return this.httpClient.get<PersonRes>(
+      `https://statsapi.web.nhl.com/api/v1/` + `${playerId}`
+    ).pipe(map(data => data.people[0]));
+  }
 
-  getYearlyData(playerId: string, year: string): Observable<StatsObj[]> {}
+  getYearlyData(playerId: string, year: string): Observable<PlayerStats> {
+
+  }
 }
