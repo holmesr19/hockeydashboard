78e5a0d840ebce19538365d23e702ec1bfd95f00
holmesr19 <holmesr19@mail.wlu.edu>
2021-03-13 18:20:40 -0500

    preliminary player game stats work

--- src/app/games/single-game/single-game.component.ts
+++ src/app/games/single-game/single-game.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GamesService } from 'src/app/services/games.service';
-import { BoxScore, Official } from 'src/app/interfaces/Game/GamesRes';
+import { BoxScore, Official, BoxAwayHome } from 'src/app/interfaces/Game/GamesRes';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-single-game',
@@ -20,6 +21,8 @@ export class SingleGameComponent implements OnInit {
   selectedTeamPlayers: any[];
   showIndividualPlayerGameStats = false;
   selectedPlayer: any;
+  homeTeamPlayers: any[];
+  awayTeamPlayers: any[];
 
   constructor(private activatedRoute: ActivatedRoute,
               private gameService: GamesService) { }
@@ -29,7 +32,24 @@ export class SingleGameComponent implements OnInit {
       this.gameId = params.get('id');
     });
     this.gameService.getGameBoxScore(this.gameId)
-    .subscribe(data => this.gameData = data);
+    .subscribe(data => {
+      this.gameData = data;
+      this.populatePlayers(this.gameData.teams);
+    });
+  }
+
+  populatePlayers(teams: BoxAwayHome) {
+    // could be more compact
+    this.homeTeamPlayers = [];
+    this.awayTeamPlayers = [];
+    const homeTeamPlayerIDs = Object.getOwnPropertyNames(teams.home.players);
+    const awayTeamPlayerIDs = Object.getOwnPropertyNames(teams.away.players);
+    homeTeamPlayerIDs.forEach(element => {
+      this.homeTeamPlayers.push(teams.home.players[element]);
+    });
+    awayTeamPlayerIDs.forEach(element => {
+      this.awayTeamPlayers.push(teams.away.players[element]);
+    });
   }
 
   triggerSummary() {
@@ -44,10 +64,10 @@ export class SingleGameComponent implements OnInit {
     this.showOfficials = false;
     this.showPlayers = true;
     this.showIndividualPlayerGameStats = false;
-    let selectedTeamPlayerIds = [];
-    let selectedTeam;
+    // let selectedTeamPlayerIds = [];
+    // let selectedTeam;
     this.selectedTeamPlayers = [];
-    switch (team) {
+/*     switch (team) {
       case ('home'): {
         console.log(team + 'players');
         selectedTeam = this.gameData.teams.home;
@@ -64,7 +84,7 @@ export class SingleGameComponent implements OnInit {
     selectedTeamPlayerIds.forEach(element => {
       this.selectedTeamPlayers.push(selectedTeam.players[element]); // will be upadted
     });
-    console.log(this.selectedTeamPlayers);
+    console.log(this.selectedTeamPlayers); */
   }
 
   triggerOfficials() {
@@ -83,9 +103,8 @@ export class SingleGameComponent implements OnInit {
     }
   }
 
-  triggerIndividualPlayerGameStats(player: number) {
+  triggerIndividualPlayerGameStats(player: any) {
     this.showIndividualPlayerGameStats = true;
-    this.selectedPlayer = this.selectedTeamPlayers[player];
-    console.log(this.selectedPlayer);
+    this.selectedPlayer = player;
 }
 }
